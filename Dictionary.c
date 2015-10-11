552b82bf3b09ba0182340767295e13583c3de357
BrunoBob <bruno@cmitess.fr>
2015-10-11 18:38:48 +0200

    Deleted some useless printf

--- Dictionary.c
+++ Dictionary.c
@@ -27,29 +27,18 @@ Boolean Empty_Dictionary(Dictionary D){
 }
 
 Boolean Exist_Word(Dictionary D, char* word){ //Require a non empty dictionary
-	
-	//DEBUG
-	printf("D->car = %c : strlen = %d : word = %s\n", D->car, strlen(word), word) ;
-	
+
 	if(strlen(word) == 0 && D->FSL->car == 42){ // If end of the word return true
 		return True;
-	}
-	
+	}	
 	D = D->FSL ; 
-	//DEBUG
-	printf("D->car = %c : strlen = %d : word = %s\n", D->car, strlen(word), word) ;
 	while(*word > D->car){
 		if(D->FBR == NULL ){
 			break;
 		}
 		D = D->FBR;
 	}
-	
-	//DEBUG
-	printf("D->car = %c : strlen = %d : word = %s\n\n", D->car, strlen(word), word) ;
-	
-	if(D->car == *word){
-		
+	if(D->car == *word){		
 		return Exist_Word(D,word+1);
 	}
 	return False;		
