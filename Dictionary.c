d4c418e4f1fdc2c0e509c21dd3c803e8d3788f4e
BrunoBob <bruno@cmitess.fr>
2015-10-11 18:36:30 +0200

    Added function Exist_word

--- Dictionary.c
+++ Dictionary.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "Dictionary.h"
 
 /* ASCII TABLE
@@ -18,6 +16,8 @@
 Dictionary Create_Dictionary(void){
 	Dictionary D ;
 	D = (Dictionary) malloc(sizeof(*D)) ;
+	D->FSL = D->FBR = NULL ;
+	D->car = 0;
 	return D ;
 }
 
@@ -26,8 +26,33 @@ Boolean Empty_Dictionary(Dictionary D){
 	return (D->FSL == NULL) ;
 }
 
-Boolean Exist_Word(Dictionary D, char* word){
+Boolean Exist_Word(Dictionary D, char* word){ //Require a non empty dictionary
 	
+	//DEBUG
+	printf("D->car = %c : strlen = %d : word = %s\n", D->car, strlen(word), word) ;
+	
+	if(strlen(word) == 0 && D->FSL->car == 42){ // If end of the word return true
+		return True;
+	}
+	
+	D = D->FSL ; 
+	//DEBUG
+	printf("D->car = %c : strlen = %d : word = %s\n", D->car, strlen(word), word) ;
+	while(*word > D->car){
+		if(D->FBR == NULL ){
+			break;
+		}
+		D = D->FBR;
+	}
+	
+	//DEBUG
+	printf("D->car = %c : strlen = %d : word = %s\n\n", D->car, strlen(word), word) ;
+	
+	if(D->car == *word){
+		
+		return Exist_Word(D,word+1);
+	}
+	return False;		
 }
 
 
